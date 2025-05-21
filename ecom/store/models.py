3fd61e694e0d55646e9d5dc9f70a74a00124724e
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-05-21 10:26:17 +0530

    Made changes in the Product model to add sale badge and new sale price in the product. Made necessary changes in the home.html file to reflect the changes using if function inside the for loop.

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -34,6 +34,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product')
+    # Add sale icon
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=9)
 
     def __str__(self):
         return self.name
