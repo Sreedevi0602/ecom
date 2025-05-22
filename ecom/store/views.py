aff8c4aef105acdcbc0535d73d537e7d4adfa147
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-05-22 09:33:01 +0530

    Login logic and logout logic completed for admin. Registration yet to be done.

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,3 +10,27 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Login Completed Successfully"))
+            return redirect('home')
+        
+        else:
+            messages.success(request, ("Something Went Wrong. Please Try Again"))
+            return redirect('login')
+        
+    else:    
+
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,("You have been Logged Out"))
+    return redirect('home')
