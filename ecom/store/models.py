ac17455208a1befe700ceb47dffecda976931851
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-05-27 16:10:11 +0530

    User profile extended by adding additional details for the user and by making a default profile creation for the user. For that created a new profile model for the user with all the needed fields and connected it with a function to automatically create and save a user profile. Later registered the profile model in the admin and wrote the code to connect the user model and profile model. Then unregistered the old User model and reregistered the new model.

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,7 +1,37 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+#Create User Profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=500, blank=True)
+    address2 = models.CharField(max_length=500, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    district = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
+    zipcode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
+#Create a user profile by default when user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance) #connecting the Profile model and the User model
+        user_profile.save()
+
+#Automate the profile thing
+post_save.connect(create_profile, sender=User)
+
+
 
 #Categories of Books
 class Category(models.Model):
