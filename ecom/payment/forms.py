852f973711a8a4a35c4ade1d579f28b32ef2abf9
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-06-10 03:55:19 +0530

    Billing information added.

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -18,3 +18,17 @@ class ShippingForm(forms.ModelForm):
         fields = ['shipping_full_name', 'shipping_email', 'shipping_phone', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_district', 'shipping_state', 'shipping_zipcode', 'shipping_country']
 
         exclude = ['user']
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name on the Card'}), required= True)
+    card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Card Number'}), required= True)
+    card_exp_date = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Expiration Date'}), required= True)
+    card_cvv_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CVV'}), required= True)
+    card_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address Line 1'}), required= True)
+    card_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address Line 2'}), required= True)
+    card_city = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}), required= True)
+    card_district = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'District'}), required= True)
+    card_state = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}), required= True)
+    card_zipcode = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zipcode'}), required= True)
+    card_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}), required= True)
+
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 
 
 
 # Create your views here.
+def billing_info(request):
+    if request.POST:
+        cart=Cart(request)
+        cart_products=cart.get_prods
+        quantities=cart.get_quants
+        totals= cart.cart_total()
+
+        #logged in user
+        if request.user.is_authenticated:
+            #get billing form
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {'cart_products':cart_products, 'quantities':quantities, 'totals': totals, 'shipping_info': request.POST, "billing_form": billing_form})
+
+        else:
+            #Guest user
+            #get billing form
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {'cart_products':cart_products, 'quantities':quantities, 'totals': totals, 'shipping_info': request.POST, "billing_form": billing_form})
+
+        
+
+    else:
+        messages.success(request, "Access to this page is denied.")
+        return redirect("home")
+    
+   
+
+
+
+
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
 
